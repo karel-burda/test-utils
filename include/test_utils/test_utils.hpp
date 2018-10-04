d00e6097c8c3d6bcc4d3589217be4f1a40e2b861
Karel Burda <karel.burda@nxp.com>
2018-10-04 15:33:22 +0200

    Added src code comment

--- include/test_utils/test_utils.hpp
+++ include/test_utils/test_utils.hpp
@@ -24,6 +24,7 @@ inline void check_if_mutex_is_owned(std::mutex & mutex, bool should_be_owned)
     const auto is_mutex_owned = !mutex.try_lock();
     EXPECT_EQ(is_mutex_owned, should_be_owned);
 
+    // if the mutex was previously not owned, we have to unlock it now (because we've performed to lock it)
     if (!is_mutex_owned)
     {
         mutex.unlock();
