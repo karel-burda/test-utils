af0a97af4eec94b40f15fb21e55580af069d8147
Karel Burda <karel.burda@nxp.com>
2018-10-05 09:34:05 +0200

    Updated tests on static class assertions

--- tests/unit/src/static_class_assertions_test.cpp
+++ tests/unit/src/static_class_assertions_test.cpp
@@ -6,7 +6,7 @@ namespace
 {
 namespace test_utils = burda::test_utils;
 
-TEST(static_class_assertions, assertions)
+TEST(static_class_assertions, constructibility_enabled)
 {
     struct struct_with_rule_of_five_enabled
     {
@@ -23,5 +23,43 @@ TEST(static_class_assertions, assertions)
     test_utils::assert_default_constructibility<struct_with_rule_of_five_enabled, true>();
     test_utils::assert_copy_constructibility<struct_with_rule_of_five_enabled, true>();
     test_utils::assert_move_constructibility<struct_with_rule_of_five_enabled, true>();
+}
+
+TEST(static_class_assertions, constructibility_disabled)
+{
+    struct struct_with_rule_of_five_disabled
+    {
+        struct_with_rule_of_five_disabled() = delete;
+        ~struct_with_rule_of_five_disabled() = default;
+
+        struct_with_rule_of_five_disabled(const struct_with_rule_of_five_disabled &) = delete;
+        struct_with_rule_of_five_disabled & operator=(const struct_with_rule_of_five_disabled &) = delete;
+
+        struct_with_rule_of_five_disabled & operator=(struct_with_rule_of_five_disabled &&) = delete;
+        struct_with_rule_of_five_disabled(struct_with_rule_of_five_disabled &&) = delete;
+    };
+
+    test_utils::assert_default_constructibility<struct_with_rule_of_five_disabled, false>();
+    test_utils::assert_copy_constructibility<struct_with_rule_of_five_disabled, false>();
+    test_utils::assert_move_constructibility<struct_with_rule_of_five_disabled, false>();
+}
+
+TEST(static_class_assertions, constructibility_mixed)
+{
+    struct struct_with_rule_of_five_mixed
+    {
+        struct_with_rule_of_five_mixed() = default;
+        ~struct_with_rule_of_five_mixed() = default;
+
+        struct_with_rule_of_five_mixed(const struct_with_rule_of_five_mixed &) = default;
+        struct_with_rule_of_five_mixed & operator=(const struct_with_rule_of_five_mixed &) = default;
+
+        struct_with_rule_of_five_mixed & operator=(struct_with_rule_of_five_mixed &&) = delete;
+        struct_with_rule_of_five_mixed(struct_with_rule_of_five_mixed &&) = delete;
+    };
+
+    test_utils::assert_default_constructibility<struct_with_rule_of_five_mixed, true>();
+    test_utils::assert_copy_constructibility<struct_with_rule_of_five_mixed, true>();
+    test_utils::assert_move_constructibility<struct_with_rule_of_five_mixed, false>();
 }
 }
