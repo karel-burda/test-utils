f81abb72eed714627217344eae1b773af220830b
Karel Burda <karel.burda@nxp.com>
2018-11-13 10:49:08 +0100

    Fixed assert macro

--- include/test_utils/macros.hpp
+++ include/test_utils/macros.hpp
@@ -3,4 +3,4 @@
 #include <gtest/gtest.h>
 
 #define BURDA_TEST_UTILS_EXPECT_XOR(condition1, condition2) EXPECT_TRUE(condition1 xor condition2);
-#define BURDA_TEST_UTILS_ASSERT_XOR(condition1, condition2) EXPECT_TRUE(condition1 xor condition2);
+#define BURDA_TEST_UTILS_ASSERT_XOR(condition1, condition2) ASSERT_TRUE(condition1 xor condition2);
