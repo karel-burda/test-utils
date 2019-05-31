825c8710027e2ef72dde8dcb594ceb87f149dea9
Karel Burda <karel.burda@nxp.com>
2019-05-31 14:13:03 +0200

    Improved src code formatting

--- include/test_utils/lifetime.hpp
+++ include/test_utils/lifetime.hpp
@@ -8,8 +8,8 @@ namespace test_utils
 {
 namespace lifetime
 {
-template <typename class_type, typename... arguments>
-void assert_construction_and_destruction(arguments... args)
+template <typename class_type, typename ... arguments>
+void assert_construction_and_destruction(arguments ... args)
 {
     ASSERT_NO_THROW(class_type instance{ std::forward<arguments>(args)... }; (void)instance);
 }
